e319b5aab38881907eacfa52819db9f63f867130
Kenneth Lai <kennethlai.ny@gmail.com>
2025-02-20 21:11:44 -0500

    Fix textarea double border

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -42,7 +42,7 @@ export const TextArea = ({ user, currentCircle }) => {
 
   return (
     <textarea
-      className="col form-control textArea--component"
+      className="col form-control no-focus textArea--component border-0 rounded-0"
       value={textArea}
       onChange={handleTextAreaChange}
       placeholder="Press enter to send..."
