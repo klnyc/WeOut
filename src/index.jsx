338c96e5b73b73f6979b9a6e772a9e9d3c5becfc
Kenneth Lai <kennethlai.ny@gmail.com>
2025-11-25 18:17:04 -0500

    Clean up unused files

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.scss";
 import App from "./App.jsx";
-import "tailwindcss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
